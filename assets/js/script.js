148e8824054be4dba86c2b482c7eaeaa7bdfc9c0
Denva <emsley3684@gmail.com>
2022-02-11 13:39:22 -0500

    service workers

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,5 @@
 require("bootstrap");
+
 const img1 = require("../../assets/img/food-table.jpg");
 const img2 = require("../../assets/img/grill.jpg");
 
--- webpack.config.js
+++ webpack.config.js
@@ -4,14 +4,19 @@ const BundleAnalyzerPlugin = require("webpack-bundle-analyzer").BundleAnalyzerPl
 
 module.exports = {
     entry: {
-        app: './assets/js/script.js',
+        app: "./assets/js/script.js",
         events: "./assets/js/events.js",
         schedule: "./assets/js/schedule.js",
         tickets: "./assets/js/tickets.js"
     },
     output: {
         filename: "[name].bundle.js",
-        path: __dirname + "/dist",
+        path: path.join(__dirname + "/dist"),
+    },
+    devServer: {
+        open: 'http://localhost:8080',
+        port: 8080,
+        static: path.join(__dirname, "/"),
     },
     module: {
         rules: [
@@ -19,14 +24,14 @@ module.exports = {
                 test: /\.jpg$/i,
                 use: [
                     {
-                        loader: 'file-loader',
+                        loader: "file-loader",
                         options: {
                             esModule: false,
                             name(file) {
                                 return "[path][name].[ext]"
                             },
-                            publicPath: function(url) {
-                                return url.replace("../", "/assets/")
+                            publicPath(url) {
+                                return url.replace('../', '/assets/');
                             }
                         }
                     },
